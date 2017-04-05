70e6666c983a01ed76eb919955eb64e615025da1
kwadrnt <kevinctse@gmail.com>
2017-04-05 12:30:19 -0700

    json accessible mag of quake1shows

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,22 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).on("ready", function() {
 
   $.ajax ({
-  	
+  	method: "GET",
+  	url: weekly_quakes_endpoint,
+  	dataType: "json",
+  	success: onSuccess,
+  	error: onError
   })
+  function onSuccess(json) {
+  	console.log(json);
+  	console.log(json.features[0].properties.mag);
+  }
+
+  function onError(xhr, status, errorThrown) {
+	alert("Sorry, there was a problem!");
+	console.log("Error: " + errorThrown);
+	console.log("Status: " + status);
+	console.dir(xhr);
+  }
 
 });
