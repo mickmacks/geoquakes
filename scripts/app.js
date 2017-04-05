4409f3e2abb4eea98ee871b076421aa0c833a618
kwadrnt <kevinctse@gmail.com>
2017-04-05 12:21:22 -0700

    initial setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
+// sanity check
+console.log('working');
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).on("ready", function() {
 
-  // CODE IN HERE!
+  $.ajax ({
+  	
+  })
 
 });
