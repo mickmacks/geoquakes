85409cd66d437ecbd64d950d5fff679226746d76
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-04-06 07:26:47 -0700

    consolidated scripts and added markers in for loop

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,22 @@ console.log('working');
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
-
 $(document).on("ready", function() {
+
+  createMap();
+
+});
+
+function createMap() {
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 37.78, lng: -122.44},
+      zoom: 2
+    });
+
+}
+
+// function getQuakeList () {
 
   $.ajax ({
   	method: "GET",
@@ -12,55 +26,44 @@ $(document).on("ready", function() {
   	dataType: "json",
   	success: onSuccess,
   	error: onError
-  })
-  function onSuccess(json) {
-  	console.log(json);
-  	console.log(json.features[0].properties.mag);
-  
-   var quakes = json.features;
-   
-   for (quake of quakes) {
+  });
 
-	// declare variable for each string
-	var mag = quake.properties.mag;
-	var place = quake.properties.place;
-	var lat = quake.geometry.coordinates[0];
-	var lng = quake.geometry.coordinates[1];
+// }
 
+function onSuccess(json) {
 
-	console.log(mag);
+  console.log(json);
+  console.log(json.features[0].properties.mag);
 
-   	$('#info').append("<p>"+ "M " + mag + " - " + place + "</p>");
+  var quakes = json.features;
 
-   	// function quakeInfo() {
+  for (quake of quakes) {
 
-   	// }
-   }
-  	
-  	
+    // declare variable for each string
+    var mag = quake.properties.mag;
+    var place = quake.properties.place;
+
+    $('#info').append("<p>" + quake.properties.title + "</p>");
+
+    // create a map marker
+    lat = quake.geometry.coordinates[1];
+    lng = quake.geometry.coordinates[0];
+
+    new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map: map,
+      title: quake.properties.title,
+    });
 
   }
 
+}
 
+function onError(xhr, status, errorThrown) {
 
-  function onError(xhr, status, errorThrown) {
 	alert("Sorry, there was a problem!");
 	console.log("Error: " + errorThrown);
 	console.log("Status: " + status);
 	console.dir(xhr);
-  }
 
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
